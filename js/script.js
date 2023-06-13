e1e1ae7a05247dd3c83821089a9316b97f72f038
Teddson <129096140+Teddson@users.noreply.github.com>
2023-06-13 12:44:29 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+//multitext
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typingEffect = new Typed(".multitext", {
+      strings: [
+        "Front-End Developer",
+        "Web Designer",
+        "UI/UX Designer",
+        "Graphics Designer",
+        "Data Entry"
+      ],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1500
+    });
+  });
+
+  //toggle icon navbar
+
